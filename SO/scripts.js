de7848f1be397d6e45f8aaa718517a2a51267865
David Elmer <davelmer@hotmail.com>
2014-11-02 23:31:51 -0500

    Added JS plus minor mods to HTML

--- SO/scripts.js
+++ SO/scripts.js
@@ -1,14 +1,72 @@
-/* I'll be messing the with some JavaScript below for experimentation. */
+var navItems = document.getElementsByTagName('h3');
 
-/*** n00bCode.v1.314
+_.each(navItems, function(navItem){
 
-var = somethingMagical
+    var isNull = _.isNull(navItem.nextSibling);
 
-function (something) {
-  if (you click on shit)
-    open.menu || close.menu.ifOpen;
-  else (don't do shit)
+    if (!isNull){
+      navItem.onclick = clickNavItem;
+    }
+});
+
+/***
+ * @param DOMevent event
+ * @return undefined
+ */
+function clickNavItem(event){
+
+   if (isClosed(this.nextSibling.nextSibling)){
+     makeEverythingClosed(navItems);
+     makeItOpen(this.nextSibling.nextSibling);
+   } else if (isOpen(this.nextSibling.nextSibling)){
+     makeItClosed(this.nextSibling.nextSibling);
+   }
+};
+
+/**
+ * @param HTMLElement element
+ * @return Boolean if `element` is "open"
+ */
+function isOpen(element){
+  return (element.className === 'open');
 }
 
+/**
+ * @param HTMLElement element
+ * @return Boolean if `element` is "closed"
+ */
+function isClosed(element){
+  return !isOpen(element);
+}
 
-*/
+/**
+ * @param HTMLElement element
+ * @return undefined
+ */
+function makeItOpen(element){
+  element.style.display = 'block';
+  element.className = 'open';
+}
+
+/**
+ * @param HTMLElement element
+ * @return undefined
+ */
+function makeItClosed(element){
+  element.style.display = 'none';
+  element.className = '';
+}
+
+/**
+ * @param Collection elements
+ * @return undefined
+ */
+function makeEverythingClosed(elements){
+
+  _.each(elements, function(element){
+      var isNull = _.isNull(element.nextSibling);
+      if (!isNull){
+        makeItClosed(element.nextSibling.nextSibling);
+      }
+  });
+}
