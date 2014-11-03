193fdd6bf3dc598697a0f6873122e3bcbf7eab57
Nicholas Hare <oharemarketing@gmail.com>
2014-11-02 19:48:00 -0500

    VICTORY NOT DEATH, PG js twirly bird win

--- PG/scripts.js
+++ PG/scripts.js
@@ -1,5 +1,4 @@
 // product grid js
-
 /*
 add onClick event to images fa-fa-repeat icon
 onClick event should give css transition class
@@ -8,46 +7,12 @@ SPAN fa-fa-repeat
 IMG  front <-next sibling
 IMG  back <-next/previous sibling
 
+Toggle? If/else? Both?
+Query? New concepts.
+Ramblings -Nhare
 */
 
-var repeatIcons = document.getElementsByTagName('span');
-      _.each(repeatIcons, function(repeatIcon){
-        repeatIcon.onclick = clickRepeatIcon;
-      });
-
 function clickRepeatIcon(event){
-    if(this.nextSibling.className == "start"){
-        this.nextSibling.className += " flipped";
-    }
-    else{
-        this.nextSibling.className = "start";
-    }
+    // console.log(this.nextSibling.nextSibling.firstChild.nextSibling);
+    this.nextSibling.nextSibling.firstChild.nextSibling.classList.toggle('awesome');
 }
-
-    // this.parentElement - apply CSS tranistion
-    // Need CSS transition call
-
-
-
-// Ramblings -Nhare
-
-//Toggle? If/else? Both?
-//Query? New concepts.
-
-
-// function flipIt (){
-//
-// }
-//
-// function makeFront(){
-//}
-//
-// function makeBack(){
-//}
-// function isFront (){
-//   return (display === 'initial');
-// }
-//
-// function isBack (){
-//   return (display === 'none');
-// }
